523f2d4d6746450b9ccd49eac8689561d3b1c736
chernic <iamchernic@gmail.com>
2014-08-05 09:39:49 +0800

    Before LOG_ERROR "Edit Test 2.0"
    LOG_ERROR "Edit Test 2.0"
    # 模拟场景二:# 为 PHP 文件的数组变量 增添 子元素(原始版)

--- FileEditTest.sh
+++ FileEditTest.sh
@@ -121,5 +121,35 @@ DIRR="/usr/share/fonts/ukai.ttc"
 			sed -n '79,90p' $FILE	
 	LOG_INFO "Change $1 Done.\n"
 }
-ChangePHP2_0 "$TEST_OT/global_settings.php" 
+# ChangePHP2_0 "$TEST_OT/global_settings.php" 
 
+ChangePHP2_1()
+{
+LOG_ERROR "Edit Test 2.1"
+# 模拟场景二:# 为 PHP 文件的数组变量 增添 子元素(中插法，不能保证max_length存在)
+# 步骤1, 定位字符串,找到首字段所在行
+# 步骤2, 模拟原字符串格式,复制字符串 直到尾字段
+# 步骤3, 修改符合格式的字符串的内容  直到尾字段
+PHP_DIR=$CACTI_LINK/include/global_settings.php
+	# 找出指定行并修改
+	LOG_INFO "Change $1 enoD"
+		FILE=$1     # "$PHP_DIR/global_settings.php"
+		BEGIN=$2    # "path_rrdtool_default_font"
+		ENDIN=$3    # "max_length"
+		CONTENT=$4  # "/usr/share/fonts/ukai.ttc"
+		LOG_INFO "  Found : $FILE"
+		
+		LOG_INFO "  Edit  : Line $ss enoD"
+			# 定位 path_rrdtool_default_font 段到 max_length 段的字符串
+			ss=$(grep -n $BEGIN $FILE | cut -d ':' -f 1)
+			# 重复 max_length 段的格式
+			sed -i $ss',/'$ENDIN'/ s/^.*'$ENDIN'.*$/&\n&/' $FILE
+			# 修改 新增的 max_length 段
+			sed -i $ss',/'$ENDIN'/ s:"'$ENDIN'.*$:"default" => "'$CONTENT'",:' $FILE
+		LOG_INFO "  Edit  : Line $ss Done\n"
+
+		LOG_INFO "  Grep  : enabled from $1"
+			sed -n '79,90p' $FILE	
+	LOG_INFO "Change $1 Done.\n"
+}
+# ChangePHP2_1 "$TEST_OT/global_settings.php"  "path_rrdtool_default_font"  "max_length"  "/usr/share/fonts/ukai.ttc"
